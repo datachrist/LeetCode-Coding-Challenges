f56072c88f63f362337d62a0d72476e2e0664e35
Shad <sam.sehgal87@gmail.com>
2017-03-12 17:01:19 -0500

    initial commit

---
+++ src/com/leetCode/arrays/longestSubString/LongestSubString.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.leetCode.arrays.longestSubString;
+
+
+public class LongestSubString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
